60d30627e67a4ec6410d3f384602e13c94b928f4
Jing Zhang <jing.zhang3@amd.com>
2019-03-22 15:56:29 -0500

    added inline assembly into thread-wise gemm

--- src/include/threadwise_gemm.hip.hpp
+++ src/include/threadwise_gemm.hip.hpp
@@ -53,17 +53,39 @@ __device__ void threadwise_gemm(MatrixA,
         constexpr unsigned N = c_mtx.NCol();
         constexpr unsigned K = a_mtx.NRow(); // A is transposed
 
+        assert(M == 8);
+        assert(N == 8);
+        assert(K == 1);
+
         for(unsigned k = 0; k < K; ++k)
         {
+            const unsigned bindex = b_mtx.Get1dIndex(k, 0);
             for(unsigned i = 0; i < M; ++i)
             {
-                for(unsigned j = 0; j < N; ++j)
-                {
-                    const unsigned aindex = a_mtx.Get1dIndex(k, i); // A is transposed
-                    const unsigned bindex = b_mtx.Get1dIndex(k, j);
-                    const unsigned cindex = c_mtx.Get1dIndex(i, j);
+                const unsigned aindex = a_mtx.Get1dIndex(k, i); // A is transposed
+                const unsigned cindex = c_mtx.Get1dIndex(i, 0);
 
-                    f_accum(p_c_thread[cindex], p_a_thread[aindex] * p_b_thread[bindex]);
+                //N = 8
+                //for(unsigned j = 0; j < N; ++j)
+                {
+                    //const unsigned bindex = b_mtx.Get1dIndex(k, j);
+                    //const unsigned cindex = c_mtx.Get1dIndex(i, j);
+                    //f_accum(p_c_thread[cindex], p_a_thread[aindex] * p_b_thread[bindex]);
+
+                    asm volatile("\n \
+                            v_mac_f32 %0, %8, %9 \n \
+                            v_mac_f32 %1, %8, %10 \n \
+                            v_mac_f32 %2, %8, %11 \n \
+                            v_mac_f32 %3, %8, %12 \n \
+                            v_mac_f32 %4, %8, %13 \n \
+                            v_mac_f32 %5, %8, %14 \n \
+                            v_mac_f32 %6, %8, %15 \n \
+                            v_mac_f32 %7, %8, %16 \n \
+                            "
+                            : "=v"(p_c_thread[cindex + 0]),"=v"(p_c_thread[cindex + 1]),"=v"(p_c_thread[cindex + 2]),"=v"(p_c_thread[cindex + 3]),"=v"(p_c_thread[cindex + 4]),"=v"(p_c_thread[cindex + 5]),"=v"(p_c_thread[cindex + 6]),"=v"(p_c_thread[cindex + 7])
+                            : "v"(p_a_thread[aindex]), "v"(p_b_thread[bindex + 0]), "v"(p_b_thread[bindex + 1]),"v"(p_b_thread[bindex + 2]),"v"(p_b_thread[bindex + 3]),"v"(p_b_thread[bindex + 4]),"v"(p_b_thread[bindex + 5]),"v"(p_b_thread[bindex + 6]),"v"(p_b_thread[bindex + 7]),
+                            "0"(p_c_thread[cindex + 0]),"1"(p_c_thread[cindex + 1]),"2"(p_c_thread[cindex + 2]),"3"(p_c_thread[cindex + 3]),"4"(p_c_thread[cindex + 4]),"5"(p_c_thread[cindex + 5]),"6"(p_c_thread[cindex + 6]),"7"(p_c_thread[cindex + 7])
+                            );
                 }
             }
         }
