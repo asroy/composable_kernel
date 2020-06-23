207021c0d28d9a685db8fcfb9b41fc2485355c59
Chao Liu <lc.roy86@gmail.com>
2020-06-23 17:44:52 -0500

    update build script

--- script/cmake-rocm3.5.sh
+++ script/cmake-rocm3.5.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 rm -f CMakeCache.txt
 rm -f *.cmake
 rm -rf CMakeFiles
@@ -11,9 +10,13 @@ cmake                                                                           
 -D CMAKE_INSTALL_PREFIX=${MY_PROJECT_INSTALL}                                               \
 -D CMAKE_BUILD_TYPE=Release                                                                 \
 -D DEVICE_BACKEND="AMD"                                                                     \
--D HIP_HIPCC_FLAGS="${HIP_HIPCC_FLAGS} -gline-tables-only -v"                               \
--D CMAKE_CXX_FLAGS="-gline-tables-only --amdgpu-target=gfx906"                              \
--D CMAKE_CXX_COMPILER=/opt/rocm/hip/bin/hipcc                                               \
+-D CMAKE_CXX_FLAGS="-O3  --amdgpu-target=gfx906 -mllvm --amdgpu-enable-global-sgpr-addr -mllvm --amdgpu-spill-vgpr-to-agpr=0"                       \
+-D CMAKE_CXX_COMPILER=/opt/rocm/bin/hipcc                                                   \
 -D CMAKE_PREFIX_PATH="/opt/rocm"                                                            \
 -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON                                                           \
 ${MY_PROJECT_SOURCE}
+
+#-D HIP_HIPCC_FLAGS="${HIP_HIPCC_FLAGS} -gline-tables-only -v"                               \
+#-D CMAKE_CXX_FLAGS="-gline-tables-only --amdgpu-target=gfx906"                              \
+
+
