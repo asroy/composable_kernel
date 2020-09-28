55d461a02c3f2f439d3bbf031f086df94e632cdf
Chao Liu <lc.roy86@gmail.com>
2020-09-28 11:12:44 -0500

    refactoring Array

--- composable_kernel/include/tensor_description/multi_index.hpp
+++ composable_kernel/include/tensor_description/multi_index.hpp
@@ -15,7 +15,7 @@ __host__ __device__ constexpr auto make_multi_index(const Xs&... xs)
 {
     return make_array<const index_t>(std::forward<const Xs>(xs)...);
 }
-#else
+#else // doesn't work: don't know how to get the lvalue/rvalue reference correct
 template <typename... Xs>
 __host__ __device__ constexpr auto make_multi_index(Xs&&... xs)
 {
