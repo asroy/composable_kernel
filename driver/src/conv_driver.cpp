1928a3d70fb48fffcb1ebd4ed6b2ebc460ef9194
Chao Liu <chao.liu2@amd.com>
2021-05-12 19:24:15 +0000

    refactor

--- driver/src/conv_driver.cpp
+++ driver/src/conv_driver.cpp
@@ -724,7 +724,7 @@ int main(int argc, char* argv[])
                                                                  LeftPads{},
                                                                  RightPads{},
                                                                  nrepeat);
-#elif 0
+#elif 1
     device_dynamic_convolution_forward_implicit_gemm_v4r4_nchw_kcyx_nkhw<in_data_t,
                                                                          in_vector_size,
                                                                          acc_data_t,
